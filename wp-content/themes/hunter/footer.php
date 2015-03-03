b0cb429ffefff5fb81732fec388ad6410ca029c7
Brycen Dorsay <dorsay@live.ca>
2015-03-02 19:14:16 -0800

    Bootstrap header and Wordpress header/footer hooks

---
+++ wp-content/themes/hunter/footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brycen
+ * Date: 2015-03-02
+ * Time: 6:22 PM
+ */
+?>
+<footer>
+    Footer
+</footer>
+
+<?php wp_footer(); ?>
+
+
+</body>
+</html>
---
+++ wp-content/themes/hunter/functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brycen
+ * Date: 2015-03-02
+ * Time: 7:01 PM
+ */
+
+function hunter_scripts()
+{
+    wp_register_script( 'jquery', get_template_directory_uri() . '/vendor/jquery/jquery-2.1.3.min.js', array(), false, true);
+    wp_register_script( 'bootstrap', get_template_directory_uri() . '/vendor/bootstrap/js/bootstrap.min.js', array(), false, true);
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'bootstrap' );
+}
+add_action( 'wp_enqueue_scripts', 'hunter_scripts' );
