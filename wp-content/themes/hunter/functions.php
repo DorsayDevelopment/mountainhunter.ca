de27c1e2354022ada2856eecf92882113bd38dd9
Brycen Dorsay <dorsay@live.ca>
2015-03-07 01:25:15 -0800

    Header logo centers on small viewport

--- wp-content/themes/hunter/functions.php
+++ wp-content/themes/hunter/functions.php
@@ -15,7 +15,10 @@ function hunter_scripts()
 }
 add_action( 'wp_enqueue_scripts', 'hunter_scripts' );
 
-function register_my_menu() {
-    register_nav_menu('header-menu',__( 'Header Menu' ));
+function register_menus() {
+    register_nav_menu('header-main-menu',__( 'Header Main Menu' ));
+    register_nav_menu('header-secondary-menu',__( 'Header Secondary Menu' ));
 }
-add_action( 'init', 'register_my_menu' );
+add_action( 'init', 'register_menus' );
+
+add_theme_support( 'woocommerce' );
--- wp-content/themes/hunter/header.php
+++ wp-content/themes/hunter/header.php
@@ -20,10 +20,15 @@
 <nav class="navbar navbar-default">
     <div class="container-fluid" id="site-logo-area">
         <div class="container">
-            <a class="home-link col-md-2" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-                <img id="site-logo" src="<?php bloginfo('template_directory')?>/img/MHS.png" alt="" class=""/>
-                <!--            <h1 class="site-title">--><?php //bloginfo( 'name' ); ?><!--</h1>-->
-            </a>
+            <div class="col-md-10 col-sm-12">
+                <a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+                    <img id="site-logo" src="<?php bloginfo('template_directory')?>/img/MHS.png" alt="" class=""/>
+                    <!--            <h1 class="site-title">--><?php //bloginfo( 'name' ); ?><!--</h1>-->
+                </a>
+            </div>
+            <div class="col-md-2 hidden-sm hidden-xs">
+                <?php wp_nav_menu( array( 'theme_location' => 'header-secondary-menu', 'menu_class' => 'nav navbar-nav' ) ); ?>
+            </div>
         </div>
     </div>
     <div class="container">
@@ -36,17 +41,11 @@
                     <span class="icon-bar"></span>
                 <?php } ?>
             </button>
-            <ul id="social-bar-outside-collapsed" class="nav navbar-nav social-icons">
-                <li><a href=""><i class="fa fa-facebook-official"></i></a></li>
-                <li><a href=""><i class="fa fa-twitter-square"></i></a></li>
-                <li><a href=""><i class="fa fa-pinterest-square"></i></a></li>
-                <li><a href=""><i class="fa fa-instagram"></i></a></li>
-            </ul>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="navbar-collapse-version">
-            <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_class' => 'nav navbar-nav' ) ); ?>
+            <?php wp_nav_menu( array( 'theme_location' => 'header-main-menu', 'menu_class' => 'nav navbar-nav' ) ); ?>
             <ul id="social-bar-inside-collapsed" class="nav navbar-nav navbar-right social-icons">
                 <li><a href=""><i class="fa fa-facebook-official"></i></a></li>
                 <li><a href=""><i class="fa fa-twitter-square"></i></a></li>
