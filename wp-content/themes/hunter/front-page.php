f00cd3fdc43a20da77364db202c40b50e46dd7a2
Brycen Dorsay <dorsay@live.ca>
2015-03-07 03:04:03 -0800

    Woo product loop on home page

--- wp-content/themes/hunter/front-page.php
+++ wp-content/themes/hunter/front-page.php
@@ -10,36 +10,62 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <div class="col-md-4 col-sm-12 hover-effect text-center">
+
+    <!--3 tabs-->
+    <div class="teaser col-md-4 col-sm-12 hover-effect text-center">
         <figure class="effect-bubba">
             <img src="<?php bloginfo('template_directory'); echo '/' ?>img/forest-24.jpg" alt="coffee cup"/>
             <figcaption>
-                <h2>New <span>Produts</span></h2>
+                <h2>New <span>Products</span></h2>
                 <p>The latest and greatest from Mountain Hunter Society</p>
                 <a href="#">View more</a>
             </figcaption>
         </figure>
     </div>
-    <div class="col-md-4 col-sm-12 hover-effect text-center">
+    <div class="teaser col-md-4 col-sm-12 hover-effect text-center">
         <figure class="effect-bubba">
             <img src="<?php bloginfo('template_directory'); echo '/' ?>img/forest-33.jpg" alt="coffee cup"/>
             <figcaption>
-                <h2>Fresh <span>Bubba</span></h2>
-                <p>Bubba likes to appear out of thin air.</p>
+                <h2>Join <span>MHS</span></h2>
+                <p>Become a member of the Society</p>
                 <a href="#">View more</a>
             </figcaption>
         </figure>
     </div>
-    <div class="col-md-4 col-sm-12 hover-effect text-center">
+    <div class="teaser col-md-4 col-sm-12 hover-effect text-center">
         <figure class="effect-bubba">
             <img src="<?php bloginfo('template_directory'); echo '/' ?>img/mountain-49.jpg" alt="coffee cup"/>
             <figcaption>
-                <h2>Fresh <span>Bubba</span></h2>
-                <p>Bubba likes to appear out of thin air.</p>
+                <h2>About <span>Us</span></h2>
+                <p>Get to know the MHS crew.</p>
                 <a href="#">View more</a>
             </figcaption>
         </figure>
     </div>
+
+
+    <div class="headline col-md-12">
+        Popular Products
+    </div>
+
+    <?php
+    $args = array( 'post_type' => 'product');
+    $loop = new WP_Query( $args );
+    while ( $loop->have_posts() ) : $loop->the_post(); ?>
+
+        <div class="col-md-3 col-sm-12 home-products">
+            <a href="<?php echo get_permalink( $loop->post->ID ) ?>">
+                <?php
+                if(has_post_thumbnail()) {
+                    the_post_thumbnail();
+                } ?>
+                <p><?php the_title(); ?></p>
+
+            </a>
+        </div>
+
+    <?php endwhile; wp_reset_query(); ?>
+
 </div>
 
 <?php get_footer(); ?>
--- wp-content/themes/hunter/page.php
+++ wp-content/themes/hunter/page.php
@@ -1,11 +1,3 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Brycen
- * Date: 2015-03-02
- * Time: 11:22 PM
- */
-?>
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
