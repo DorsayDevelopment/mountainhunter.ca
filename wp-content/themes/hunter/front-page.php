50625f46e2db771527f22a1ce6608eccd354a366
Brycen Dorsay <dorsay@live.ca>
2015-03-07 02:00:42 -0800

    Mousovers on homepage

--- wp-content/themes/hunter/front-page.php
+++ wp-content/themes/hunter/front-page.php
@@ -9,6 +9,37 @@
 
 <?php get_header(); ?>
 
-The home page
+<div class="container">
+    <div class="col-md-4 col-sm-12 hover-effect text-center">
+        <figure class="effect-bubba">
+            <img src="<?php bloginfo('template_directory'); echo '/' ?>img/forest-24.jpg" alt="coffee cup"/>
+            <figcaption>
+                <h2>New <span>Produts</span></h2>
+                <p>The latest and greatest from Mountain Hunter Society</p>
+                <a href="#">View more</a>
+            </figcaption>
+        </figure>
+    </div>
+    <div class="col-md-4 col-sm-12 hover-effect text-center">
+        <figure class="effect-bubba">
+            <img src="<?php bloginfo('template_directory'); echo '/' ?>img/forest-33.jpg" alt="coffee cup"/>
+            <figcaption>
+                <h2>Fresh <span>Bubba</span></h2>
+                <p>Bubba likes to appear out of thin air.</p>
+                <a href="#">View more</a>
+            </figcaption>
+        </figure>
+    </div>
+    <div class="col-md-4 col-sm-12 hover-effect text-center">
+        <figure class="effect-bubba">
+            <img src="<?php bloginfo('template_directory'); echo '/' ?>img/mountain-49.jpg" alt="coffee cup"/>
+            <figcaption>
+                <h2>Fresh <span>Bubba</span></h2>
+                <p>Bubba likes to appear out of thin air.</p>
+                <a href="#">View more</a>
+            </figcaption>
+        </figure>
+    </div>
+</div>
 
 <?php get_footer(); ?>
