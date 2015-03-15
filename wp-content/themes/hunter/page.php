0d6388dc636e704f5deabf82634c0f515b435ee8
Brycen Dorsay <dorsay@live.ca>
2015-03-15 01:17:42 -0700

    Bootstrapped about page

--- wp-content/themes/hunter/page.php
+++ wp-content/themes/hunter/page.php
@@ -1,12 +1,22 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-9">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>
 
-<?php endwhile; else: ?>
-    <p><?php _e('Sorry, this page does not exist.'); ?></p>
-<?php endif; ?>
+            <?php endwhile; else: ?>
+                <p><?php _e('Sorry, this page does not exist.'); ?></p>
+            <?php endif; ?>
+        </div>
+        <div class="col-md-3">
+            <?php do_action( 'woocommerce_sidebar' ); ?>
+        </div>
+    </div>
+</div>
+
 
 
 <?php get_footer(); ?>
