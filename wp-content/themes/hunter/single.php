600785886a1dd8cc1ed1f980efd592e3e92f9b40
Brycen Dorsay <dorsay@live.ca>
2015-03-15 01:15:55 -0700

    Bootstrapped single blog post

--- wp-content/themes/hunter/single.php
+++ wp-content/themes/hunter/single.php
@@ -8,15 +8,26 @@
 ?>
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-9">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>
 
-    <?php comments_template(); ?>
+                <?php comments_template(); ?>
 
-<?php endwhile; else: ?>
-    <p><?php _e('Sorry, this post does not exist.'); ?></p>
-<?php endif; ?>
+            <?php endwhile; else: ?>
+                <p><?php _e('Sorry, this post does not exist.'); ?></p>
+            <?php endif; ?>
+        </div>
+        <div class="col-md-3">
+            <?php do_action( 'woocommerce_sidebar' ); ?>
+        </div>
+    </div>
+
+</div>
+
 
 
 <?php get_footer(); ?>
