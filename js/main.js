b0f8fea462f477cae9fe2acfc2ac8224dd7d6db5
Brycen <dorsay@live.ca>
2016-05-11 21:54:53 -0700

    Sticky nav

--- js/main.js
+++ js/main.js
@@ -2,13 +2,15 @@ $j=jQuery.noConflict();
 
 $j(document).ready(function() {
     $j('.button-collapse').sideNav();
-    
-    
+
+
+    var navTop = $j('nav').offset().top;
+
     $j(window).scroll(function () {
-        if ($j(window).scrollTop() > 50) {
+        if ($j(window).scrollTop() > navTop) {
             $j('nav').addClass('nav-fixed');
         }
-        if ($j(window).scrollTop() < 51) {
+        if ($j(window).scrollTop() < navTop) {
             $j('nav').removeClass('nav-fixed');
         }
     });
