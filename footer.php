7fce4a3dfcb38fb6d1a451d91efc409a5c21519e
Brycen <dorsay@live.ca>
2016-05-11 21:31:31 -0700

    Footer beginnings

--- footer.php
+++ footer.php
@@ -1,5 +1,15 @@
 
 footer
 <?php wp_footer(); ?>
+
+<footer>
+  <div class="footer-logo">
+    <img src="<?php bloginfo('template_directory') ?>/img/MHS.png" alt="">
+  </div>
+  <div class="footer-byline">
+    &copy; Mountain Hunter Society <?php echo getdate()["year"]; ?> | Site by <a href="http://www.dorsaydevelopment.ca">Dorsay Development</a>
+  </div>
+</footer>
+
 </body>
 </html>
