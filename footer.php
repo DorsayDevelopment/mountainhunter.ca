3bc9d23564d51186cc9c70bcfb1d9655df3d65c1
Brycen <dorsay@live.ca>
2016-05-12 04:27:09 -0700

    Fix header x overflow and copyright date

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
     <a href="#"><img src="<?php bloginfo('template_directory') ?>/img/MHS_dark.png" alt=""></a>
   </div>
   <div class="footer-byline">
-    &copy; Mountain Hunter Society <?php echo getdate()["year"]; ?> | Site by&nbsp;<a href="http://dorsaydevelopment.ca">Dorsay Development</a>
+    &copy; Mountain Hunter Society <?php echo getdate()['year'] ?> | Site by&nbsp;<a href="http://dorsaydevelopment.ca">Dorsay Development</a>
   </div>
 </footer>
 
