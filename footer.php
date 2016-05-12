419b49f98c2595d8c54710c85855148e12f20a7a
Brycen <dorsay@live.ca>
2016-05-12 01:29:49 -0700

    Woocommerce loop on front page

--- footer.php
+++ footer.php
@@ -1,10 +1,8 @@
-
-footer
 <?php wp_footer(); ?>
 
 <footer>
   <div class="footer-logo">
-    <img src="<?php bloginfo('template_directory') ?>/img/MHS.png" alt="">
+    <a href="#"><img src="<?php bloginfo('template_directory') ?>/img/MHS.png" alt=""></a>
   </div>
   <div class="footer-byline">
     &copy; Mountain Hunter Society <?php echo getdate()["year"]; ?> | Site by &nbsp;<a href="http://www.dorsaydevelopment.ca">Dorsay Development</a>
--- front-page.php
+++ front-page.php
@@ -1,9 +1,31 @@
 <div id="headliner">
-  <img src="<?php bloginfo('template_directory') ?>/img/MHS.png" alt="">
+  <a href="#"><img src="<?php bloginfo('template_directory') ?>/img/MHS.png" alt=""></a>
 </div>
 
 <?php get_header(); ?>
 
-main
+<div class="heading">
+  <h3>Featured</h3>
+</div>
+
+<div class="product-list">
+  <?php
+  $args = array( 'post_type' => 'product', 'product_cat' => 'featured', 'numberposts' => 3);
+  $loop = new WP_Query($args);
+  while ( $loop->have_posts() ) : $loop->the_post(); ?>
+
+    <div class="product-thumbnail">
+      <a href="<?php echo get_permalink( $loop->post->ID ) ?>">
+        <?php
+        if(has_post_thumbnail()) {
+          the_post_thumbnail();
+        } ?>
+        <p><?php the_title(); ?></p>
+      </a>
+    </div>
+
+  <?php endwhile; wp_reset_query(); ?>
+</div>
+
 
 <?php get_footer(); ?>
